83b3d8a9419252005435e56e0dc81bb647a31e70
한규빈 <rbsks147@daum.net>
2023-03-21 23:30:19 +0900

    IoC/DI 스프링 적용

--- src/one/DaoFactory.java
+++ src/one/DaoFactory.java
@@ -1,13 +1,21 @@
 package one;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// 애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정정보라는 표시
+@Configuration
 public class DaoFactory {
 
+    // 오브젝트 생성을 담당하는 IoC용 메소드라는 표시
+    @Bean
     public UserDao userDao() {
         UserDao userDao = new UserDao(getConnectionMaker());
         return userDao;
     }
 
-    private ConnectionMaker getConnectionMaker() {
+    @Bean
+    public ConnectionMaker getConnectionMaker() {
         ConnectionMaker connectionMaker = new DConnectionMaker();
         return connectionMaker;
     }
--- src/one/Main.java
+++ src/one/Main.java
@@ -1,25 +1,37 @@
 package one;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.sql.SQLException;
 
 public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
 //        UserDao userDao = new UserDao(new DConnectionMaker());
-        UserDao userDao = new DaoFactory().userDao();
+//        UserDao userDao = new DaoFactory().userDao();
 
-        User user = new User();
-        user.setId("rbsksbb");
-        user.setName("한규빈");
-        user.setPassword("11111");
+//        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+//        UserDao userDao = context.getBean("userDao", UserDao.class);
 
-        userDao.add(user);
+//        User user = new User();
+//        user.setId("rbskszz");
+//        user.setName("한규빈");
+//        user.setPassword("11111");
+//
+//        userDao.add(user);
+//
+//        System.out.println(user.getId() + " 등록 성공");
+//
+//        User getUser = userDao.get(user.getId());
+//        System.out.println(getUser.getName());
+//        System.out.println(getUser.getPassword());
+//        System.out.println(getUser.getId() + " 조회 성공");
 
-        System.out.println(user.getId() + " 등록 성공");
-
-        User getUser = userDao.get(user.getId());
-        System.out.println(getUser.getName());
-        System.out.println(getUser.getPassword());
-        System.out.println(getUser.getId() + " 조회 성공");
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao userDao1 = context.getBean("userDao", UserDao.class);
+        UserDao userDao2 = context.getBean("userDao", UserDao.class);
+        System.out.println(userDao1);
+        System.out.println(userDao2);
     }
 }
